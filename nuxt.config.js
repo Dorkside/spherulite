082e17b2b22e9f4438fc0d69489a4964d6349bd7
Dorkside <jdmartin@gmx.fr>
2022-01-23 15:42:16 +0100

    fix: update base

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,8 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  base: '/Dorkside/spherulite-test/',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'spherulite',
